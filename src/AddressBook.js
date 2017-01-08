f12a0357114c9d0dc5db95eb035ab2c31ee3e761
Andrew Winterbotham <awinterbotham02@gmail.com>
2017-01-08 22:16:22 +0000

    complete course

---
+++ src/AddressBook.js
@@ -0,0 +1,29 @@
+function AddressBook() {
+	this.contacts = [];
+	this.initialComplete = false;
+}
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+	var self = this;
+
+	setTimeout(function() {
+		 self.initialComplete = true;
+		 if (cb) {
+		 	return cb();
+		 }
+	});
+};
+
+
+AddressBook.prototype.addContact = function(contact) {
+	this.contacts.push(contact);
+};
+
+
+AddressBook.prototype.getContact = function(index) {
+	return this.contacts[index];
+};
+
+AddressBook.prototype.deleteContact = function(index) {
+	this.contacts.splice(index, 1);
+};
